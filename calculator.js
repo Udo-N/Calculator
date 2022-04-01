f496919d27bb7978b4ba69825ce6dc9548786577
Udo-N <udoka404@gmail.com>
2022-04-01 11:04:22 -0400

    Removed unnecessary code

--- calculator.js
+++ calculator.js
@@ -2,13 +2,13 @@ function array2DToHTML(array) {
     var t = "";
     for (var i = 0; i < array.length; i++){
         var cells = "<div id='row-" + (i+1) + "'>";
-        cells += "<button id='cell-"+ i +"-0'>" + array[i][0] + "</button>";
-        cells += "<button id='cell-"+ i +"-1'>" + array[i][1] + "</button>";
-        cells += "<button id='cell-"+ i +"-2'>" + array[i][2] + "</button>";
-        cells += "<button id='cell-"+ i +"-3'>" + array[i][3] + "</button>";
+
+        for (var j = 0; j < array[0].length; j++){
+            cells += "<button id='cell-"+ i +"-" + j + "'>" + array[i][j] + "</button>";
+        }
+
         cells += "</div>";
         t += cells;
-        console.log(cells)
     }
     document.getElementById("grid").innerHTML = t;
 
@@ -68,19 +68,3 @@ var inputNumbers = []
 var inputOperations = []
 
 array2DToHTML(twoDArray)
-
-document.addEventListener("DOMContentLoaded", () => {
-    const newGame = document.querySelector(".new-game");
-    const simpGame = document.querySelector(".simp-game");
-    var bclicks = document.querySelectorAll("#puzzle button");
-    
-    
-
-    for (let bclick of bclicks) {
-        bclick.addEventListener("click", () => {
-            var v = bclick.textContent;
-            moveCell(v);
-        });
-    }    
-    
-});
